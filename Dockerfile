e5631a7ccbe922b1a866ce34e7867fc9ec09866a
Jethro Kuan <jethrokuan95@gmail.com>
2018-02-22 20:41:47 +0800

    Add gensim install to docker

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,7 @@
 FROM jupyter/tensorflow-notebook
+
+RUN conda install --quiet --yes \
+    'gensim' && \
+    conda clean -tipsy && \
+    fix-permissions $CONDA_DIR && \
+    fix-permissions /home/$NB_USER
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo $(pwd)
-sudo docker run -it --rm -p 8888:8888 -v $(pwd):/home/jovyan/work jupyter/tensorflow-notebook start.sh jupyter lab
+sudo docker run -it --rm -p 8888:8888 -v $(pwd):/home/jovyan/work jethro/jupyter start.sh jupyter lab
