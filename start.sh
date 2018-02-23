cf1a1a28bbbebcbeb6c594175d4cb1ad2c1b3739
Jethro Kuan <jethrokuan95@gmail.com>
2018-02-23 09:20:56 +0800

    mount data folder too

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it --rm -p 8888:8888 -v $(pwd):/home/jovyan/work jethro/jupyter start.sh jupyter lab
+docker run -it --rm -p 8888:8888 -v $(pwd):/home/jovyan/work -v $(pwd)/data:/home/jovyan/work/data jethro/jupyter start.sh jupyter lab
